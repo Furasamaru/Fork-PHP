8f4da9e157c9cd51e535bcc2efd22f29b7204a53
Samuel <samuel.duperry@coda-student.school>
2025-11-25 16:55:55 +0100

    essais

--- 002-php-chifoumi/app/index.php
+++ 002-php-chifoumi/app/index.php
@@ -1,3 +1,36 @@
 <?php
 
-echo "Hello world";
+$possibleChoice = ["rock", "paper", "scissors"];
+$phpChoice = array_rand($possibleChoice);
+
+$rules = [
+    "rock"     => ["scissors"],
+    "paper"    => ["rock"],
+    "scissors" => ["paper"]
+];
+
+$AI = $possibleChoice;
+
+if ($choice = "pierre") {
+    $_GET["choice"] = $choice;
+}
+
+$page = <<<HTML
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Rock Paper Scissor</title>
+</head>
+    <body>
+        <main>
+            <h1>Rock Paper Scissor</h1>
+            <a href="?choice=rock" class="button">Rock</a>
+            <a href="?choice=paper" class="button">Paper</a>
+            <a href="?choice=scissor" class="button">Scissor</a>
+        </main>
+    </body>
+</html>
+HTML;
+
+echo $page;
