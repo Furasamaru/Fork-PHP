9fd5271d293dbd57490061ec48def1ec9d59d028
Samuel <samuel.duperry@coda-student.school>
2025-11-26 19:25:08 +0100

    Rock Paper Scissors without css

--- 002-php-chifoumi/app/index.php
+++ 002-php-chifoumi/app/index.php
@@ -1,18 +1,39 @@
 <?php
 
+//choix possible pour php
 $possibleChoice = ["rock", "paper", "scissors"];
-$phpChoice = array_rand($possibleChoice);
 
+//règles du pierre feuille ciseaux
 $rules = [
     "rock"     => ["scissors"],
     "paper"    => ["rock"],
     "scissors" => ["paper"]
 ];
 
-$AI = $possibleChoice;
+//différents types de choix:
+$userChoice = "";
+/*$AIChoice = array_rand($possibleChoice);*/
+$AIChoice = $possibleChoice[array_rand($possibleChoice)];
+$emptyChoice = null;
 
-if ($choice = "pierre") {
-    $_GET["choice"] = $choice;
+$result = "";
+
+//GET côté php
+if (isset($_GET["userChoice"])) {
+    //gain de temps et plus de compréhension en créant le $userGetChoice
+    $userGetChoice = $_GET["userChoice"];
+    //tous les choix possibles
+    if (!in_array($userGetChoice, $possibleChoice)) {
+        echo "Raté petit malin";
+    } else if ($userGetChoice == $emptyChoice || $AIChoice == $emptyChoice) {
+        echo "Faites un choix :";
+    } else if ($userGetChoice === $AIChoice) {
+        $result = "match nul";
+    } else if (in_array($userGetChoice, $rules[$AIChoice])) {
+        $result = "Vous avez gagné";
+    } else {
+        $result = "Vous avez perdu";
+    }
 }
 
 $page = <<<HTML
@@ -24,10 +45,13 @@ $page = <<<HTML
 </head>
     <body>
         <main>
+        <form method="get" action="index.php">
             <h1>Rock Paper Scissor</h1>
-            <a href="?choice=rock" class="button">Rock</a>
-            <a href="?choice=paper" class="button">Paper</a>
-            <a href="?choice=scissor" class="button">Scissor</a>
+            <a href="?userChoice=rock" class="button">Rock</a>
+            <a href="?userChoice=paper" class="button">Paper</a>
+            <a href="?userChoice=scissors" class="button">Scissors</a>
+            </form>
+            <h2>$result</h2>
         </main>
     </body>
 </html>
